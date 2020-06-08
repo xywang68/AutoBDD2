024085ccb28aa0c4b00db984589ff3bd8ee43ce3
xywang68 <xywang68@gmail.com>
2020-06-08 10:53:20 -0700

    add wdio hooks

--- framework/support/module_hooks.js
+++ framework/support/module_hooks.js
@@ -5,6 +5,43 @@ if (process.env.PROJECTRUNPATH) {
   projectHooks = null;
 }
 exports.hooks = {
+
+  onPrepare: function (config, capabilities) {
+    if (projectHooks) projectHooks.onPrepare(config, capabilities);
+  },
+
+  onWorkerStart: function (cid, caps, specs, args, execArgv) {
+    if (projectHooks) projectHooks.onWorkerStart(cid, caps, specs, args, execArgv);
+  },
+
+  beforeSession: function (config, capabilities, specs) {
+    if (projectHooks) projectHooks.beforeSession(config, capabilities, specs);
+  },
+
+  before: function (capabilities, specs) {
+    if (projectHooks) projectHooks.before(capabilities, specs);
+  },
+
+  beforeSuite: function (suite) {
+    if (projectHooks) projectHooks.beforeSuite(suite);
+  },
+
+  beforeHook: function (test, context/*, stepData, world*/) {
+    if (projectHooks) projectHooks.beforeHook(test, context/*, stepData, world*/);
+  },
+
+  afterHook: function (test, context, { error, result, duration, passed, retries }/*, stepData, world*/) {
+    if (projectHooks) projectHooks.afterHook(test, context, { error, result, duration, passed, retries }/*, stepData, world*/);
+  },
+
+  beforeTest: function (test, context) {
+    if (projectHooks) projectHooks.beforeTest(test, context);
+  },
+
+  beforeCommand: function (commandName, args) {
+    if (projectHooks) projectHooks.beforeCommand(commandName, args);
+  },
+
   beforeFeature: function (uri, feature, scenarios) {
     if (projectHooks) projectHooks.BeforeFeature(feature);
   },
@@ -27,5 +64,34 @@ exports.hooks = {
 
   afterFeature: function (uri, feature, scenarios) {
     if (projectHooks) projectHooks.AfterFeature(feature);
-  }
+  },
+
+  afterCommand: function (commandName, args, result, error) {
+    if (projectHooks) projectHooks.afterCommand(commandName, args, result, error);
+  },
+
+  afterTest: function (test, context, { error, result, duration, passed, retries }) {
+    if (projectHooks) projectHooks.afterTest(test, context, { error, result, duration, passed, retries });
+  },
+
+  afterSuite: function (suite) {
+    if (projectHooks) projectHooks.afterSuite(suite);
+  },
+
+  after: function (result, capabilities, specs) {
+    if (projectHooks) projectHooks.after(result, capabilities, specs);
+  },
+
+  afterSession: function (config, capabilities, specs) {
+    if (projectHooks) projectHooks.afterSession(config, capabilities, specs);
+  },
+
+  onComplete: function (exitCode, config, capabilities, results) {
+    if (projectHooks) projectHooks.onComplete(exitCode, config, capabilities, results);
+  },
+
+  onReload: function(oldSessionId, newSessionId) {
+    if (projectHooks) projectHooks.onReload(oldSessionId, newSessionId);
+  },
+
 }
